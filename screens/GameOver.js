86e7831423a5cc823ac0be684ca5c8d0df0e1b0b
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-10-30 13:11:30 -0300

    Estilizando imagens

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -7,7 +7,10 @@ const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <TitleText>O jogo acabou!</TitleText>
-      <Image source={require("../assets/success.png")} />
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../assets/success.png")} />
+      </View>
+
       <BodyText>Número de Rodadas: {props.turnos}</BodyText>
       <BodyText>O número foi: {props.numero}</BodyText>
       <Button title="Novo Jogo" onPress={props.restart} />
@@ -20,6 +23,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    marginVertical: 30
   }
 });
 
