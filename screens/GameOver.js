2853d044dc32a11a847ff068ccac8286a4fc3a74
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-11-06 07:30:21 -0300

    Calculando tamanhos dinamicamente

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  ScrollView
+} from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
 import Colors from "../constants/color";
@@ -7,35 +14,40 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = props => {
   return (
-    <View style={styles.screen}>
-      <TitleText>O jogo acabou!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={require("../assets/success.png")} />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>O jogo acabou!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={require("../assets/success.png")}
+          />
+        </View>
+        <View style={styles.resolveContainer}>
+          <BodyText style={styles.test}>
+            O seu celular precisou de
+            <Text style={styles.highlight}> {props.turnos}</Text> rodadas para
+            adivinhar o número{" "}
+            <Text style={styles.highlight}>{props.numero}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.restart}>Novo Jogo</MainButton>
       </View>
-      <View style={styles.resolveContainer}>
-        <BodyText style={styles.test}>
-          O seu celular precisou de
-          <Text style={styles.highlight}> {props.turnos}</Text> rodadas para
-          adivinhar o número{" "}
-          <Text style={styles.highlight}>{props.numero}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={props.restart}>Novo Jogo</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   test: {
     textAlign: "center",
-    fontSize: 20
+    fontSize: Dimensions.get("window").height < 400 ? "16" : 20
   },
   highlight: {
     color: Colors.primary,
     fontFamily: "open-sans-bold"
   },
   resolveContainer: {
-    marginHorizontal: 30,
+    marginHorizontal: Dimensions.get("window").height / 60,
     marginVertical: 15
   },
   screen: {
@@ -48,13 +60,13 @@ const styles = StyleSheet.create({
     height: "100%"
   },
   imageContainer: {
-    width: 300,
-    height: 300,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
     overflow: "hidden",
-    borderRadius: 150,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
     borderColor: "black",
-    marginVertical: 30
+    marginVertical: Dimensions.get("window").height / 40
   }
 });
 
