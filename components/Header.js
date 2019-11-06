8f79920d940ebcda6866f318f34b4ff09f754e7c
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-11-06 08:53:17 -0300

    Introduzindo a API de plataforma

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import Colors from "../constants/color";
 import TitleText from "../components/TitleText";
 
 const Header = props => {
   return (
     <View style={styles.header}>
-      <TitleText>{props.title}</TitleText>
+      <TitleText style={styles.title}>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  title: {
+    color: Platform.OS === "ios" ? Colors.primary : "white"
+  },
   header: {
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary,
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    borderBottomColor: Platform.OS === "ios" ? "#ccc" : "white",
+    borderBottomWidth: Platform.OS === "ios" ? 1 : 0
   }
 });
 export default Header;
