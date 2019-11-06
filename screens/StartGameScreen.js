451e74131f529d3f70fdd3dd726396c1b4e5a4b3
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-11-06 07:14:55 -0300

    Introduzindo o Dimensions API

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -8,7 +8,8 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert
+  Alert,
+  Dimensions
 } from "react-native";
 import Colors from "../constants/color";
 import Input from "../components/Input";
@@ -123,7 +124,7 @@ const styles = StyleSheet.create({
     marginVertical: 10
   },
   button: {
-    width: 100
+    width: Dimensions.get("window").width / 4
   },
   input: {
     width: 50,
